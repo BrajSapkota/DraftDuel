485b4b0b4a94988c97de121c2432608673a51be5
Braj.S <159206352+BrajSapkota@users.noreply.github.com>
2026-08-11 09:54:31 +1000

    updated spotify client id

--- spotify-config.js
+++ spotify-config.js
@@ -1,4 +1,4 @@
 // Paste the Client ID from your Spotify Developer Dashboard below.
 // Never put a Spotify Client Secret in this website or GitHub repository.
-export const SPOTIFY_CLIENT_ID = "963e7edb7bff4eefbd3043a539248dbb";
+export const SPOTIFY_CLIENT_ID = "f4fb9ea93b5540b191a6449ecc16a8b7";
 export const SPOTIFY_REDIRECT_URI = "https://brajsapkota.github.io/DraftDuel/";
