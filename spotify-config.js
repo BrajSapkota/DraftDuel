7a2341ac790092a1b5051684565f4515b207baa2
Braj.S <159206352+BrajSapkota@users.noreply.github.com>
2026-08-10 13:57:31 +1000

    Added Avatars, Spotify connection

---
+++ spotify-config.js
@@ -0,0 +1,4 @@
+// Paste the Client ID from your Spotify Developer Dashboard below.
+// Never put a Spotify Client Secret in this website or GitHub repository.
+export const SPOTIFY_CLIENT_ID = "963e7edb7bff4eefbd3043a539248dbb";
+export const SPOTIFY_REDIRECT_URI = "https://brajsapkota.github.io/DraftDuel/";
